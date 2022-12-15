b973c3f71ec7279b23cf4f343b3eb4843db648cc
Gongdongjang <jeesoo0908@gmail.com>
2022-12-15 20:27:06 +0900

    [Bug] handle my_goal error

--- controllers/my_goal.js
+++ controllers/my_goal.js
@@ -4,14 +4,15 @@ const pool = require("../db.js");
 
 router.get("/", async (req, res, next) => {
   const nickname = req.session.user["nickname"];
-  try{
-   const my_goal = await pool.query(`SELECT * FROM goal WHERE goal_user = ?`, [
-    nickname,
-  ]); }
-  catch (err) {
+  try {
+    const my_goal = await pool.query(`SELECT * FROM goal WHERE goal_user = ?`, [
+      nickname,
+    ]);
+
+    res.render("my_goal", {title: "내 목표", my_goal: my_goal[0]});
+  } catch (err) {
     console.error(err);
   }
-  res.render("my_goal", {title: "내 목표", my_goal: my_goal[0]});
 });
 
 module.exports = router;
