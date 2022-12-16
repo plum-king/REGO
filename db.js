8dcfeee6b21dac90347c9ce02e01b8406b25217d
Jisoo Hwang <77599304+plum-king@users.noreply.github.com>
2022-12-16 17:14:05 +0900

    Update db.js

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ const mysql = require("mysql2/promise");
 dotenv.config();
 
 const pool = mysql.createPool({
-  host     : 127.0.0.1,
+  host     : "127.0.0.1",
   user     : process.env.DB_USER,
   password : process.env.DB_PWD,
   database : process.env.DB_NAME,
